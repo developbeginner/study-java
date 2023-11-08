af5af339f017aebd90ecd54346535a2c3e6d5973
developbeginner <148424130+developbeginner@users.noreply.github.com>
2023-11-08 20:44:55 +0900

    Study: 빌더 공부 커밋
    - 빌더 예제 코드 작성
    - 문제풀이 코드 작성
    
    Releated to: #1

---
+++ design-pattern/src/main/java/developbeginner/builder/Main.java
@@ -0,0 +1,31 @@
+package developbeginner.builder;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            usage();
+            System.exit(0);
+        }
+        if (args[0].equals("text")) {
+            TextBuilder tb = new TextBuilder();
+            Director director = new Director(tb);
+            director.construct();
+            String result = tb.getTextResult();
+            System.out.println(result);
+        } else if (args[0].equals("html")) {
+            HTMLBuilder hb = new HTMLBuilder();
+            Director director = new Director(hb);
+            director.construct();
+            String filename = hb.getHTMLResult();
+            System.out.println("HTML파일 " + filename + "이 작성되었습니다.");
+        } else {
+            usage();
+            System.exit(0);
+        }
+    }
+
+    public static void usage() {
+        System.out.println("Usage: java Main text       텍스트로 문서 작성");
+        System.out.println("Usage: java Main text       HTML 파일로 문서 작성");
+    }
+}
